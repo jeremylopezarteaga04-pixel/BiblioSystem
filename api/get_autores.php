01c5740444340b37696d0cc26f3d223ce130e646
jeremy lopez <jeremylopezarteaga04@gmail.com>
2026-08-23 13:49:32 -0500

    Create get_autores.php
    Signed-off-by: jeremy lopez <jeremylopezarteaga04@gmail.com>

---
+++ api/get_autores.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/helpers.php';
+require_once __DIR__ . '/../config/database.php';
+
+try {
+    $consulta = $conexion->query('SELECT * FROM autores ORDER BY nombre');
+    responder(['success' => true, 'data' => $consulta->fetchAll(PDO::FETCH_ASSOC)]);
+} catch (Throwable $error) {
+    responder(['success' => false, 'message' => mensaje_error($error)], 500);
+}
