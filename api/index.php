fe3589f28fe4ec788315be827f8c78d44b63e9c4
jeremy lopez <jeremylopezarteaga04@gmail.com>
2026-08-23 13:46:03 -0500

    Update index.php
    Signed-off-by: jeremy lopez <jeremylopezarteaga04@gmail.com>

--- api/index.php
+++ api/index.php
@@ -1,21 +1,29 @@
 <?php
-// api/index.php
-header('Content-Type: application/json; charset=utf-8');
+require_once __DIR__ . '/helpers.php';
 
-echo json_encode([
-    "success" => true,
-    "message" => "Bienvenido a la API de BiblioSystem",
-    "version" => "1.0",
-    "endpoints_disponibles" => [
-        "GET" => [
-            "obtener_usuarios" => "api/obtener_usuarios.php",
-            "obtener_prestamos" => "api/obtener_prestamos.php"
+responder([
+    'success' => true,
+    'message' => 'Bienvenido a la API de BiblioSystem',
+    'version' => '2.0',
+    'endpoints_disponibles' => [
+        'GET' => [
+            'libros' => 'get_libros.php',
+            'usuarios' => 'get_usuarios.php',
+            'prestamos' => 'get_prestamos.php',
+            'categorias' => 'get_categorias.php',
+            'autores' => 'get_autores.php',
+            'bitacora' => 'get_bitacora.php',
         ],
-        "POST" => [
-            "registrar_usuario" => "api/registrar_usuario.php",
-            "registrar_libro" => "api/registrar_libro.php",
-            "registrar_prestamo" => "api/registrar_prestamo.php"
-        ]
-    ]
-], JSON_UNESCAPED_UNICODE);
-?>
+        'POST' => [
+            'registrar_libro' => 'registrar_libro.php',
+            'actualizar_libro' => 'actualizar_libro.php',
+            'eliminar_libro' => 'eliminar_libro.php',
+            'registrar_usuario' => 'registrar_usuario.php',
+            'actualizar_usuario' => 'actualizar_usuario.php',
+            'registrar_prestamo' => 'registrar_prestamo.php',
+            'devolver_prestamo' => 'devolver_prestamo.php',
+            'registrar_categoria' => 'registrar_categoria.php',
+            'registrar_autor' => 'registrar_autor.php',
+        ],
+    ],
+]);
