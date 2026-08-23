a6c9781b70c6722a03a48539b808d2059dd51d7d
jeremy lopez <jeremylopezarteaga04@gmail.com>
2026-08-23 14:53:02 -0500

    Update get_autores.php
    Signed-off-by: jeremy lopez <jeremylopezarteaga04@gmail.com>

--- api/get_autores.php
+++ api/get_autores.php
@@ -1,23 +1,10 @@
 <?php
 require_once __DIR__ . '/helpers.php';
-exigir_post();
 require_once __DIR__ . '/../config/database.php';
 
 try {
-    $cedula = dato('cedula');
-    $nombres = dato('nombres');
-    $apellidos = dato('apellidos');
-    $correo = dato('correo');
-
-    if (!$cedula || !$nombres || !$apellidos || !filter_var($correo, FILTER_VALIDATE_EMAIL)) {
-        throw new Exception('Ingresa cédula, nombres, apellidos y un correo electrónico válido.');
-    }
-
-    $sql = 'INSERT INTO usuarios (cedula, nombres, apellidos, correo, telefono, direccion) VALUES (?, ?, ?, ?, ?, ?)';
-    $conexion->prepare($sql)->execute([$cedula, $nombres, $apellidos, $correo, dato('telefono') ?: null, dato('direccion') ?: null]);
-    $id = (int) $conexion->lastInsertId();
-    registrar_bitacora($conexion, 'REGISTRAR USUARIO', 'Se registró el lector ' . $nombres . ' ' . $apellidos, 'usuarios', $id);
-    responder(['success' => true, 'message' => 'Lector registrado exitosamente.', 'id' => $id]);
+    $consulta = $conexion->query('SELECT * FROM autores ORDER BY nombre');
+    responder(['success' => true, 'data' => $consulta->fetchAll(PDO::FETCH_ASSOC)]);
 } catch (Throwable $error) {
-    responder(['success' => false, 'message' => mensaje_error($error)], 400);
+    responder(['success' => false, 'message' => mensaje_error($error)], 500);
 }
